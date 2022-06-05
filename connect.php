b688a9da253a631c64fe81e64f7b9bdc34d0191e
ItsDan2311 <firedan01@gmail.com>
2022-06-05 21:11:55 +0100

    testing

---
+++ connect.php
@@ -0,0 +1,36 @@
+<?php
+$BusinessName = $_POST['bname'];
+$StreetName = $_POST['sname'];
+$AlreadyMember = $_POST['amember'];
+$Comments = $_POST['comments'];
+$StreetName = $_POST['sname'];
+$Date = $_POST['date'];
+if (!empty($username)) {
+    if (!empty($password)) {
+        $host = "sql4.freesqldatabase.com";
+        $dbusername = "sql4497902";
+        $dbpassword = "wBbtRDUBrp";
+        $dbname = "sql4497902";
+        // Create connection
+        $conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
+        if (mysqli_connect_error()) {
+            die('Connect Error (' . mysqli_connect_errno() . ') '
+                . mysqli_connect_error());
+        } else {
+            $sql = "INSERT INTO Onstreet Tracker (Business Name, Street Name, Already Members, Comments, Date)
+            values ('$BusinessName','$StreetName','$AlreadyMember','$Comments','$StreetName','$Date')";
+            if ($conn->query($sql)) {
+                echo "New record is inserted sucessfully";
+            } else {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+            }
+            $conn->close();
+        }
+    } else {
+        echo "Password should not be empty";
+        die();
+    }
+} else {
+    echo "Username should not be empty";
+    die();
+}
